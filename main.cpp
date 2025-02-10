d5c1dd260f154d708d4da8d5ceec1af276cd8e2e
Zackary Scholl <zack.scholl@gmail.com>
2025-02-10 10:24:06 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,82 @@
+
+#include "core_cm7.h"
+#include "daisy_pod.h"
+#include "daisysp.h"
+
+uint8_t DMA_BUFFER_MEM_SECTION buffer_spi[4];
+#define INCLUDE_AUDIO_PROFILING 1
+#define RP2040_I2C_ADDRESS 0x28
+#define AUDIO_BLOCK_SIZE 128
+#define AUDIO_SAMPLE_RATE 48000
+#define CROSSFADE_PREROLL 4800
+#define MAX_SIZE 15897648
+#define CYCLES_AVAILBLE \
+  1066666  // (400000000 * AUDIO_BLOCK_SIZE / AUDIO_SAMPLE_RATE)
+using namespace daisy;
+using namespace daisysp;
+
+DaisyPod hw;
+DaisySeed daisyseed;
+Metro print_timer;
+
+size_t audiocallback_sample_num = 0;
+float cpu_needed = 0.0f;
+float cpu_max_needed = 0.0f;
+uint32_t audiocallback_time_needed = 0;
+float cpu_usage_running[30] = {0};
+size_t cpu_usage_index = 0;
+float audiocallback_bufin[AUDIO_BLOCK_SIZE * 2];
+float audiocallback_bufout[AUDIO_BLOCK_SIZE * 2];
+float inl[AUDIO_BLOCK_SIZE];
+float inr[AUDIO_BLOCK_SIZE];
+float outl[AUDIO_BLOCK_SIZE];
+float outr[AUDIO_BLOCK_SIZE];
+static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
+                          AudioHandle::InterleavingOutputBuffer out,
+                          size_t size) {
+#ifdef INCLUDE_AUDIO_PROFILING
+  // measure - start
+  DWT->CYCCNT = 0;
+  audiocallback_sample_num = size / 2;
+#endif
+
+  if (print_timer.Process()) {
+    // print hello
+    daisyseed.PrintLine("cpu needed: %2.1f", cpu_needed);
+  }
+
+#ifdef INCLUDE_AUDIO_PROFILING
+  audiocallback_time_needed = DWT->CYCCNT;
+  cpu_needed = (float)audiocallback_time_needed / CYCLES_AVAILBLE * 100.0f;
+#endif
+}
+
+int main(void) {
+  // hw.Init(false): 74.7% avg, 90.3% max
+  // hw.Init(true): 79.9% avg, 96.6% max
+  hw.Init(false);
+  daisyseed.StartLog(false);
+
+#ifdef INCLUDE_AUDIO_PROFILING
+  // setup measurement
+  // https://forum.electro-smith.com/t/solved-how-to-do-mcu-utilization-measurements/1236
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+  DWT->LAR = 0xC5ACCE55;
+  DWT->CYCCNT = 0;
+  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+#endif
+
+  // erase buf
+  memset(audiocallback_bufin, 0, sizeof(audiocallback_bufin));
+  memset(audiocallback_bufout, 0, sizeof(audiocallback_bufout));
+
+  print_timer.Init(1.0f, AUDIO_SAMPLE_RATE / AUDIO_BLOCK_SIZE);
+
+  System::Delay(2000);
+
+  hw.SetAudioBlockSize(AUDIO_BLOCK_SIZE);
+  hw.StartAudio(AudioCallback);
+
+  while (1) {
+  }
+}
