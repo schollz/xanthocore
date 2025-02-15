7a3c0b8d79066ed13f8439ac980ab4a004d177ad
Zack Scholl <zack.scholl@gmail.com>
2025-02-14 20:38:09 -0800

    feat: Add voices

---
+++ lib/softcut/Voices.cpp
@@ -0,0 +1,132 @@
+
+#include "Voices.h"
+
+#include <cstring>
+
+#define VOICES_TAPE_BUFFER 3.0f
+using namespace softcut;
+
+Voices::Voices() {}
+
+void Voices::init(float *tape, unsigned int numFrames, float sr) {
+  bufFrames = numFrames;
+  sampleRate = sr;
+
+  for (size_t i = 0; i < NUM_VOICES; i++) {
+    loopStart[i] = 0;
+    loopEnd[i] = 2;
+    panning[i] = 0.1;  // this will be set
+    inputBus[i] = 0;
+    voices[i].reset();
+    voices[i].setBuffer(tape, numFrames);
+    voices[i].setSampleRate(sampleRate);
+    voices[i].setRate(1.0);
+    voices[i].setFadeTime(0.1);
+    voices[i].setRecLevel(1.0);
+    voices[i].setPreLevel(0.75);
+    voices[i].setRecPreSlewTime(0.5);
+    voices[i].setRateSlewTime(0.5);
+    voices[i].cutToPos(1.0f);
+    voices[i].setRecOnceFlag(false);
+    voices[i].setLoopFlag(false);
+    voices[i].setRecFlag(false);
+    voices[i].setPlayFlag(false);
+    setTape(i, i);
+    setPan(i, 0.0f);
+    setLevel(i, 1.0f);
+    setLoopStart(i, 0.0f);
+    setLoopEnd(i, 2.0f);
+    cutToPos(i, 0.0f);
+  }
+}
+
+void Voices::setTape(size_t voice, size_t index) {
+  float totalSeconds = (float)bufFrames / (float)sampleRate;
+  totalSeconds = totalSeconds - (VOICES_TAPE_BUFFER * 2);
+  tapeSliceStart[voice] = (float)index / (float)NUM_VOICES * totalSeconds;
+  tapeSliceEnd[voice] = (float)(index + 1) / (float)NUM_VOICES * totalSeconds;
+  setLoopStart(voice, loopStart[voice]);
+  setLoopEnd(voice, loopEnd[voice]);
+}
+
+void Voices::setRate(size_t voice, float rate) { voices[voice].setRate(rate); }
+
+void Voices::setLoopStart(size_t voice, float sec) {
+  loopStart[voice] = sec;
+  float start = VOICES_TAPE_BUFFER + loopStart[voice] + tapeSliceStart[voice];
+  voices[voice].setLoopStart(start);
+}
+
+void Voices::setLoopEnd(size_t voice, float sec) {
+  loopEnd[voice] = sec;
+  voices[voice].setLoopEnd(VOICES_TAPE_BUFFER + loopEnd[voice] +
+                           tapeSliceStart[voice]);
+}
+
+void Voices::cutToPos(size_t voice, float sec) {
+  voices[voice].cutToPos(3 + sec + tapeSliceStart[voice]);
+}
+
+void Voices::setLoopFlag(size_t voice, bool val) {
+  voices[voice].setLoopFlag(val);
+}
+
+void Voices::setRecFlag(size_t voice, bool val) {
+  voices[voice].setRecFlag(val);
+}
+
+void Voices::setRecOnceFlag(size_t voice, bool val) {
+  voices[voice].setRecOnceFlag(val);
+}
+
+void Voices::setPlayFlag(size_t voice, bool val) {
+  voices[voice].setPlayFlag(val);
+}
+
+void Voices::setPan(size_t voice, float pan) {
+  pan = fclamp(pan, -1.0f, 1.0f);
+  if (pan == panning[voice]) {
+    return;
+  }
+  panning[voice] = pan;
+  panningL[voice] = sinf(0.25f * M_PI * (1.0f - pan));
+  panningR[voice] = cosf(0.25f * M_PI * (1.0f - pan));
+}
+
+void Voices::setLevel(size_t voice, float level) {
+  level = fclamp(level, 0.0f, 2.0f);
+  levels[voice] = level / (float)NUM_VOICES;
+}
+
+void Voices::setDB(size_t voice, float db) { setLevel(voice, dbamp(db)); }
+
+void Voices::setPreLevel(size_t voice, float val) {
+  voices[voice].setPreLevel(val);
+}
+
+void Voices::setRecLevel(size_t voice, float val) {
+  voices[voice].setRecLevel(val);
+}
+
+void Voices::process(const float *inl, const float *inr, float *outl,
+                     float *outr, unsigned int numFrames) {
+  for (size_t i = 0; i < NUM_VOICES; i++) {
+    if (voices[i].getPlayFlag() == false) {
+      continue;
+    }
+    float output[numFrames];
+    memset(output, 0, numFrames * sizeof(float));
+    if (i == 0) {
+      if (inputBus[i] == 0) {
+        voices[i].processBlockMono(inl, output, numFrames);
+      } else {
+        voices[i].processBlockMono(inr, output, numFrames);
+      }
+    }
+    for (size_t j = 0; j < numFrames; j++) {
+      output[j] *= levels[i];
+      outl[j] += output[j] * panningL[i];
+      outr[j] += output[j] * panningR[i];
+    }
+  }
+}
---
+++ lib/softcut/Voices.h
@@ -0,0 +1,50 @@
+#ifndef Softcut_Voices_H
+#define Softcut_Voices_H
+
+#include "Voice.h"
+
+#define NUM_VOICES 4
+
+namespace softcut {
+class Voices {
+ public:
+  Voices();
+  void init(float *buf, unsigned int numFrames, float sampleRate);
+  void setTape(size_t voice, size_t index);
+  void setRate(size_t voice, float rate);
+  void setDB(size_t voice, float db);
+  void setLevel(size_t voice, float level);
+  void setPan(size_t voice, float pan);
+  void setLoopStart(size_t voice, float sec);
+  void setLoopEnd(size_t voice, float sec);
+  void setLoopFlag(size_t voice, bool val);
+  void setRecFlag(size_t voice, bool val);
+  void setRecOnceFlag(size_t voice, bool val);
+  void setPlayFlag(size_t voice, bool val);
+  void setPreLevel(size_t voice, float val);
+  void setRecLevel(size_t voice, float val);
+  void cutToPos(size_t voice, float sec);
+  void process(const float *inl, const float *inr, float *outl, float *outr,
+               unsigned int numFrames);
+
+ private:
+  Voice voices[NUM_VOICES];
+  float tapeSliceStart[NUM_VOICES];
+  float tapeSliceEnd[NUM_VOICES];
+  float loopStart[NUM_VOICES];
+  float loopEnd[NUM_VOICES];
+  float panning[NUM_VOICES];
+  float panningL[NUM_VOICES];
+  float panningR[NUM_VOICES];
+  float levels[NUM_VOICES];
+  float inputBus[NUM_VOICES];
+  bool playing[NUM_VOICES];
+
+  float *buf;
+  unsigned int bufFrames;
+  float sampleRate;
+};
+
+}  // namespace softcut
+
+#endif
